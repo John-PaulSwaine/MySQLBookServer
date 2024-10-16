9b4544355ef30ed09e25cb114b3b89c6dda95ddf
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-16 14:26:58 +0100

    completed book routes and pulled into server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 require ('dotenv').config()
 const express = require ('express')
 const app = express()
-app.use(express.json())
+const bookRouter = require('./db/routes/bookRoutes')
+const Book = require('./db/models/bookModel')
 
+const syncTables = () => {
+    Book.sync({
+        alter: true
+    })
+}
+
+app.use(express.json())
+app.use(bookRouter)
 
 app.get('/health', (req, res)=>{
     res.status(200).send('API is healthy')
 })
+
+syncTables()
 
 app.listen(5001, ()=>{
     console.log('server listening on port 5001.')
