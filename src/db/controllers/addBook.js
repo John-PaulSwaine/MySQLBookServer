37b46959f67fd24709591fa92ff0c75c5e6cc8bc
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-16 14:34:42 +0100

    completed addBook functionality

--- src/db/controllers/addBook.js
+++ src/db/controllers/addBook.js
@@ -1,10 +1,17 @@
+const Book = require('../models/bookModel')
 const bookModel = require('../models/bookModel')
 
 const addBook = async (req, res) => {
     try {
-        
+        const output = await Book.create({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre
+        })
+        res.status(400).json({msg: `${req.body.title} by ${req.body.author} has successfully been added.`})
+        console.log(output)
     } catch (error) {
-        res.status(418)
+        res.status(418).json(error)
         console.log(error)
     }
 }
