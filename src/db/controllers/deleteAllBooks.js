a06075440bf8a7f373d8769d2c278ed3cdfa4a21
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-18 11:11:44 +0100

    completed functionality for deleteAllBooks.js

--- src/db/controllers/deleteAllBooks.js
+++ src/db/controllers/deleteAllBooks.js
@@ -1,12 +1,16 @@
-const Book = require('../models/bookModel')
+const Book = require('../models/bookModel');
 
 const deleteAllBooks = async (req, res) => {
     try {
-
+        const output = await Book.destroy({
+            where: {},
+            truncate: true // This option ensures that the table is reset and the auto-increment counter is reset
+        });
+        res.status(200).json({ message: 'All books deleted successfully', deleted: output });
     } catch (error) {
-        res.status(418).json(error)
-        console.log(error)
+        res.status(418).json(error);
+        console.log(error);
     }
-}
+};
 
-module.exports = deleteAllBooks
+module.exports = deleteAllBooks;
